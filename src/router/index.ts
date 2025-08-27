054865193abeb70998dd0c45a5792b586aecff36
Yuki <134689144+yuki10000@users.noreply.github.com>
2025-08-27 13:42:23 +0900

    #3 ページのタイトルを動的に生成するようにした

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -7,18 +8,32 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: () => import('../pages/HomeView.vue'),
+      meta: { title: 'ホーム' },
     },
     {
       path: '/exercise',
       name: 'Exercise',
       component: () => import('../pages/ExerciseView.vue'),
+      meta: { title: '演習' },
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
       component: () => import('../pages/NotFound.vue'),
+      meta: { title: 'ページが見つかりません' },
     },
   ],
 })
+
+// ページタイトルを動的に変更
+router.beforeEach((to, from, next) => {
+  const baseTitle = '段階的翻訳演習';
+  if (to.meta && to.meta.title) {
+    document.title = `${baseTitle} | ${to.meta.title}`;
+  } else {
+    document.title = baseTitle;
+  }
+  next();
+});
 
 export default router
